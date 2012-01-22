f94faf087a291809d43fac692d85fc07999d2e65
Sven Hodapp <sven.hodapp@gmail.com>
2012-01-22 15:28:54 +0100

    added scalate library. can now be imported in the scala-files.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,3 +1,14 @@
+import org.fusesource.scalate._
+
 object Hello {
-  def main(args: Array[String]) = println("Hello World!")
+  def main(args: Array[String]) {
+    println("Hello World!")
+    print(this.tmpl)
+  }
+
+  def tmpl : String = {
+    val engine = new TemplateEngine
+    val output = engine.layout("test.scaml")
+    return output
+  }
 }
